97227c2272573f15ab702e8600a6cb6496129c49
michael.ziso <michael.ziso@taboola.com>
2026-07-12 22:43:45 +0300

    Add final scenarios landing page

---
+++ public/js/final-scenarios-snapshot.js
@@ -0,0 +1,701 @@
+window.FINAL_SCENARIO_SNAPSHOT = {
+  "version": "2026-07-12T040532Z",
+  "scenarios": [
+    {
+      "id": "france-england-france",
+      "index": 0,
+      "semiOneWinner": "France",
+      "semiTwoWinner": "England",
+      "champion": "France",
+      "finalists": [
+        "France",
+        "England"
+      ],
+      "leader": {
+        "name": "N_Melech3",
+        "projected": 689.56
+      },
+      "rows": [
+        {
+          "name": "N_Melech3",
+          "currentPoints": 589.56,
+          "added": 100,
+          "projected": 689.56,
+          "projectedRank": 1
+        },
+        {
+          "name": "RanStern",
+          "currentPoints": 587,
+          "added": 100,
+          "projected": 687,
+          "projectedRank": 2
+        },
+        {
+          "name": "GuyNaimRoy",
+          "currentPoints": 568,
+          "added": 100,
+          "projected": 668,
+          "projectedRank": 3
+        },
+        {
+          "name": "N_Karin2",
+          "currentPoints": 567.56,
+          "added": 100,
+          "projected": 667.56,
+          "projectedRank": 4
+        },
+        {
+          "name": "IdoKoish",
+          "currentPoints": 561,
+          "added": 100,
+          "projected": 661,
+          "projectedRank": 5
+        },
+        {
+          "name": "YogevTzur2",
+          "currentPoints": 594,
+          "added": 60,
+          "projected": 654,
+          "projectedRank": 6
+        },
+        {
+          "name": "N_TalEden",
+          "currentPoints": 547.56,
+          "added": 100,
+          "projected": 647.56,
+          "projectedRank": 7
+        },
+        {
+          "name": "N_Arsenal2",
+          "currentPoints": 586.56,
+          "added": 60,
+          "projected": 646.56,
+          "projectedRank": 8
+        },
+        {
+          "name": "sharonWisman",
+          "currentPoints": 574,
+          "added": 70,
+          "projected": 644,
+          "projectedRank": 9
+        },
+        {
+          "name": "RoeiMelech",
+          "currentPoints": 541,
+          "added": 100,
+          "projected": 641,
+          "projectedRank": 10
+        }
+      ]
+    },
+    {
+      "id": "france-england-england",
+      "index": 1,
+      "semiOneWinner": "France",
+      "semiTwoWinner": "England",
+      "champion": "England",
+      "finalists": [
+        "France",
+        "England"
+      ],
+      "leader": {
+        "name": "YogevTzur2",
+        "projected": 694
+      },
+      "rows": [
+        {
+          "name": "YogevTzur2",
+          "currentPoints": 594,
+          "added": 100,
+          "projected": 694,
+          "projectedRank": 1
+        },
+        {
+          "name": "N_Arsenal2",
+          "currentPoints": 586.56,
+          "added": 100,
+          "projected": 686.56,
+          "projectedRank": 2
+        },
+        {
+          "name": "Sagi1",
+          "currentPoints": 567,
+          "added": 100,
+          "projected": 667,
+          "projectedRank": 3
+        },
+        {
+          "name": "N_Melech3",
+          "currentPoints": 589.56,
+          "added": 60,
+          "projected": 649.56,
+          "projectedRank": 4
+        },
+        {
+          "name": "RanStern",
+          "currentPoints": 587,
+          "added": 60,
+          "projected": 647,
+          "projectedRank": 5
+        },
+        {
+          "name": "RoeiNaim",
+          "currentPoints": 573,
+          "added": 70,
+          "projected": 643,
+          "projectedRank": 6
+        },
+        {
+          "name": "GuyNaimRoy",
+          "currentPoints": 568,
+          "added": 60,
+          "projected": 628,
+          "projectedRank": 7
+        },
+        {
+          "name": "dordagan",
+          "currentPoints": 558,
+          "added": 70,
+          "projected": 628,
+          "projectedRank": 7
+        },
+        {
+          "name": "Carmit-romi",
+          "currentPoints": 528,
+          "added": 100,
+          "projected": 628,
+          "projectedRank": 7
+        },
+        {
+          "name": "N_Karin2",
+          "currentPoints": 567.56,
+          "added": 60,
+          "projected": 627.56,
+          "projectedRank": 10
+        }
+      ]
+    },
+    {
+      "id": "france-argentina-france",
+      "index": 2,
+      "semiOneWinner": "France",
+      "semiTwoWinner": "Argentina",
+      "champion": "France",
+      "finalists": [
+        "France",
+        "Argentina"
+      ],
+      "leader": {
+        "name": "EyalRoei2",
+        "projected": 663
+      },
+      "rows": [
+        {
+          "name": "EyalRoei2",
+          "currentPoints": 563,
+          "added": 100,
+          "projected": 663,
+          "projectedRank": 1
+        },
+        {
+          "name": "N_Melech3",
+          "currentPoints": 589.56,
+          "added": 70,
+          "projected": 659.56,
+          "projectedRank": 2
+        },
+        {
+          "name": "RanStern",
+          "currentPoints": 587,
+          "added": 70,
+          "projected": 657,
+          "projectedRank": 3
+        },
+        {
+          "name": "sharonWisman",
+          "currentPoints": 574,
+          "added": 70,
+          "projected": 644,
+          "projectedRank": 4
+        },
+        {
+          "name": "GuyNaimRoy",
+          "currentPoints": 568,
+          "added": 70,
+          "projected": 638,
+          "projectedRank": 5
+        },
+        {
+          "name": "N_Karin2",
+          "currentPoints": 567.56,
+          "added": 70,
+          "projected": 637.56,
+          "projectedRank": 6
+        },
+        {
+          "name": "PelegCohen1",
+          "currentPoints": 564,
+          "added": 70,
+          "projected": 634,
+          "projectedRank": 7
+        },
+        {
+          "name": "Pini2",
+          "currentPoints": 532,
+          "added": 100,
+          "projected": 632,
+          "projectedRank": 8
+        },
+        {
+          "name": "IdoKoish",
+          "currentPoints": 561,
+          "added": 70,
+          "projected": 631,
+          "projectedRank": 9
+        },
+        {
+          "name": "GOALBALL",
+          "currentPoints": 557,
+          "added": 70,
+          "projected": 627,
+          "projectedRank": 10
+        }
+      ]
+    },
+    {
+      "id": "france-argentina-argentina",
+      "index": 3,
+      "semiOneWinner": "France",
+      "semiTwoWinner": "Argentina",
+      "champion": "Argentina",
+      "finalists": [
+        "France",
+        "Argentina"
+      ],
+      "leader": {
+        "name": "N_ GuyNaimRoy2",
+        "projected": 661.56
+      },
+      "rows": [
+        {
+          "name": "N_ GuyNaimRoy2",
+          "currentPoints": 561.56,
+          "added": 100,
+          "projected": 661.56,
+          "projectedRank": 1
+        },
+        {
+          "name": "NirCohen2",
+          "currentPoints": 550,
+          "added": 100,
+          "projected": 650,
+          "projectedRank": 2
+        },
+        {
+          "name": "N_RoeiVainshtein",
+          "currentPoints": 543.56,
+          "added": 100,
+          "projected": 643.56,
+          "projectedRank": 3
+        },
+        {
+          "name": "N_NirPeleg",
+          "currentPoints": 565.56,
+          "added": 70,
+          "projected": 635.56,
+          "projectedRank": 4
+        },
+        {
+          "name": "YogevTzur2",
+          "currentPoints": 594,
+          "added": 30,
+          "projected": 624,
+          "projectedRank": 5
+        },
+        {
+          "name": "EyalRoei2",
+          "currentPoints": 563,
+          "added": 60,
+          "projected": 623,
+          "projectedRank": 6
+        },
+        {
+          "name": "N_Melech3",
+          "currentPoints": 589.56,
+          "added": 30,
+          "projected": 619.56,
+          "projectedRank": 7
+        },
+        {
+          "name": "RoeiGuy",
+          "currentPoints": 548,
+          "added": 70,
+          "projected": 618,
+          "projectedRank": 8
+        },
+        {
+          "name": "RanStern",
+          "currentPoints": 587,
+          "added": 30,
+          "projected": 617,
+          "projectedRank": 9
+        },
+        {
+          "name": "N_Arsenal2",
+          "currentPoints": 586.56,
+          "added": 30,
+          "projected": 616.56,
+          "projectedRank": 10
+        }
+      ]
+    },
+    {
+      "id": "spain-england-spain",
+      "index": 4,
+      "semiOneWinner": "Spain",
+      "semiTwoWinner": "England",
+      "champion": "Spain",
+      "finalists": [
+        "Spain",
+        "England"
+      ],
+      "leader": {
+        "name": "GuyNaim",
+        "projected": 677
+      },
+      "rows": [
+        {
+          "name": "GuyNaim",
+          "currentPoints": 577,
+          "added": 100,
+          "projected": 677,
+          "projectedRank": 1
+        },
+        {
+          "name": "Crazy1",
+          "currentPoints": 564,
+          "added": 100,
+          "projected": 664,
+          "projectedRank": 2
+        },
+        {
+          "name": "ItayGriner",
+          "currentPoints": 575,
+          "added": 70,
+          "projected": 645,
+          "projectedRank": 3
+        },
+        {
+          "name": "RoeiNaim",
+          "currentPoints": 573,
+          "added": 60,
+          "projected": 633,
+          "projectedRank": 4
+        },
+        {
+          "name": "YogevTzur2",
+          "currentPoints": 594,
+          "added": 30,
+          "projected": 624,
+          "projectedRank": 5
+        },
+        {
+          "name": "N_Melech3",
+          "currentPoints": 589.56,
+          "added": 30,
+          "projected": 619.56,
+          "projectedRank": 6
+        },
+        {
+          "name": "dordagan",
+          "currentPoints": 558,
+          "added": 60,
+          "projected": 618,
+          "projectedRank": 7
+        },
+        {
+          "name": "RanStern",
+          "currentPoints": 587,
+          "added": 30,
+          "projected": 617,
+          "projectedRank": 8
+        },
+        {
+          "name": "N_Arsenal2",
+          "currentPoints": 586.56,
+          "added": 30,
+          "projected": 616.56,
+          "projectedRank": 9
+        },
+        {
+          "name": "AlonAfek",
+          "currentPoints": 552,
+          "added": 60,
+          "projected": 612,
+          "projectedRank": 10
+        }
+      ]
+    },
+    {
+      "id": "spain-england-england",
+      "index": 5,
+      "semiOneWinner": "Spain",
+      "semiTwoWinner": "England",
+      "champion": "England",
+      "finalists": [
+        "Spain",
+        "England"
+      ],
+      "leader": {
+        "name": "RoeiNaim",
+        "projected": 673
+      },
+      "rows": [
+        {
+          "name": "RoeiNaim",
+          "currentPoints": 573,
+          "added": 100,
+          "projected": 673,
+          "projectedRank": 1
+        },
+        {
+          "name": "YogevTzur2",
+          "currentPoints": 594,
+          "added": 70,
+          "projected": 664,
+          "projectedRank": 2
+        },
+        {
+          "name": "dordagan",
+          "currentPoints": 558,
+          "added": 100,
+          "projected": 658,
+          "projectedRank": 3
+        },
+        {
+          "name": "N_Arsenal2",
+          "currentPoints": 586.56,
+          "added": 70,
+          "projected": 656.56,
+          "projectedRank": 4
+        },
+        {
+          "name": "AlonAfek",
+          "currentPoints": 552,
+          "added": 100,
+          "projected": 652,
+          "projectedRank": 5
+        },
+        {
+          "name": "LiavShemesh",
+          "currentPoints": 552,
+          "added": 100,
+          "projected": 652,
+          "projectedRank": 5
+        },
+        {
+          "name": "GuyNaim",
+          "currentPoints": 577,
+          "added": 60,
+          "projected": 637,
+          "projectedRank": 7
+        },
+        {
+          "name": "Sagi1",
+          "currentPoints": 567,
+          "added": 70,
+          "projected": 637,
+          "projectedRank": 7
+        },
+        {
+          "name": "Crazy1",
+          "currentPoints": 564,
+          "added": 60,
+          "projected": 624,
+          "projectedRank": 9
+        },
+        {
+          "name": "GuyNaim2",
+          "currentPoints": 524,
+          "added": 100,
+          "projected": 624,
+          "projectedRank": 9
+        }
+      ]
+    },
+    {
+      "id": "spain-argentina-spain",
+      "index": 6,
+      "semiOneWinner": "Spain",
+      "semiTwoWinner": "Argentina",
+      "champion": "Spain",
+      "finalists": [
+        "Spain",
+        "Argentina"
+      ],
+      "leader": {
+        "name": "ItayGriner",
+        "projected": 675
+      },
+      "rows": [
+        {
+          "name": "ItayGriner",
+          "currentPoints": 575,
+          "added": 100,
+          "projected": 675,
+          "projectedRank": 1
+        },
+        {
+          "name": "GuyNaim",
+          "currentPoints": 577,
+          "added": 70,
+          "projected": 647,
+          "projectedRank": 2
+        },
+        {
+          "name": "Crazy1",
+          "currentPoints": 564,
+          "added": 70,
+          "projected": 634,
+          "projectedRank": 3
+        },
+        {
+          "name": "OmriMilner",
+          "currentPoints": 534,
+          "added": 100,
+          "projected": 634,
+          "projectedRank": 3
+        },
+        {
+          "name": "PelegCohen2",
+          "currentPoints": 531,
+          "added": 100,
+          "projected": 631,
+          "projectedRank": 5
+        },
+        {
+          "name": "N_NirPeleg",
+          "currentPoints": 565.56,
+          "added": 60,
+          "projected": 625.56,
+          "projectedRank": 6
+        },
+        {
+          "name": "RoyBarak",
+          "currentPoints": 520,
+          "added": 100,
+          "projected": 620,
+          "projectedRank": 7
+        },
+        {
+          "name": "Shachar",
+          "currentPoints": 510,
+          "added": 100,
+          "projected": 610,
+          "projectedRank": 8
+        },
+        {
+          "name": "RoeiGuy",
+          "currentPoints": 548,
+          "added": 60,
+          "projected": 608,
+          "projectedRank": 9
+        },
+        {
+          "name": "LiatCohen",
+          "currentPoints": 537,
+          "added": 70,
+          "projected": 607,
+          "projectedRank": 10
+        }
+      ]
+    },
+    {
+      "id": "spain-argentina-argentina",
+      "index": 7,
+      "semiOneWinner": "Spain",
+      "semiTwoWinner": "Argentina",
+      "champion": "Argentina",
+      "finalists": [
+        "Spain",
+        "Argentina"
+      ],
+      "leader": {
+        "name": "N_NirPeleg",
+        "projected": 665.56
+      },
+      "rows": [
+        {
+          "name": "N_NirPeleg",
+          "currentPoints": 565.56,
+          "added": 100,
+          "projected": 665.56,
+          "projectedRank": 1
+        },
+        {
+          "name": "RoeiGuy",
+          "currentPoints": 548,
+          "added": 100,
+          "projected": 648,
+          "projectedRank": 2
+        },
+        {
+          "name": "ItayGriner",
+          "currentPoints": 575,
+          "added": 60,
+          "projected": 635,
+          "projectedRank": 3
+        },
+        {
+          "name": "N_ GuyNaimRoy2",
+          "currentPoints": 561.56,
+          "added": 70,
+          "projected": 631.56,
+          "projectedRank": 4
+        },
+        {
+          "name": "NirCohen2",
+          "currentPoints": 550,
+          "added": 70,
+          "projected": 620,
+          "projectedRank": 5
+        },
+        {
+          "name": "N_RoeiVainshtein",
+          "currentPoints": 543.56,
+          "added": 70,
+          "projected": 613.56,
+          "projectedRank": 6
+        },
+        {
+          "name": "AmirLin",
+          "currentPoints": 508,
+          "added": 100,
+          "projected": 608,
+          "projectedRank": 7
+        },
+        {
+          "name": "GuyNaim",
+          "currentPoints": 577,
+          "added": 30,
+          "projected": 607,
+          "projectedRank": 8
+        },
+        {
+          "name": "RoeiNaim",
+          "currentPoints": 573,
+          "added": 30,
+          "projected": 603,
+          "projectedRank": 9
+        },
+        {
+          "name": "Pini3",
+          "currentPoints": 568,
+          "added": 30,
+          "projected": 598,
+          "projectedRank": 10
+        }
+      ]
+    }
+  ]
+};
---
+++ public/js/final-scenarios.js
@@ -0,0 +1,171 @@
+(function () {
+  const CARD_ACCENTS = ["#63c7ff", "#f4c84b", "#37d67a", "#f15b4a"];
+
+  const state = {
+    scenarios: [],
+    activeId: "",
+  };
+
+  const els = {
+    scenarioGrid: document.getElementById("scenarioGrid"),
+    activeFinal: document.getElementById("activeFinal"),
+    activeChampion: document.getElementById("activeChampion"),
+    versionStamp: document.getElementById("versionStamp"),
+    toast: document.getElementById("toast"),
+    copyPageBtn: document.getElementById("copyPageBtn"),
+  };
+
+  function fmt(points) {
+    return Number(points).toFixed(2).replace(/\.00$/, "");
+  }
+
+  function shortVersion(version) {
+    const match = String(version || "").match(/^(\d{4})-(\d{2})-(\d{2})/);
+    if (!match) return "Latest";
+    const date = new Date(`${match[1]}-${match[2]}-${match[3]}T00:00:00Z`);
+    return date.toLocaleDateString("en-US", { month: "short", day: "numeric" });
+  }
+
+  function html(text) {
+    const div = document.createElement("div");
+    div.textContent = String(text ?? "");
+    return div.innerHTML;
+  }
+
+  function teamMarkup(teamName) {
+    const flag = typeof flagHtml === "function" ? flagHtml(teamName, "sm") : "";
+    return `${flag}<span class="team-name">${html(teamName)}</span>`;
+  }
+
+  function renderTeamLines() {
+    document.querySelectorAll("[data-team-line]").forEach((el) => {
+      el.innerHTML = teamMarkup(el.dataset.teamLine);
+    });
+  }
+
+  function scenarioCard(scenario) {
+    const finalistText = `${scenario.finalists[0]} x ${scenario.finalists[1]}`;
+    const leader = scenario.leader;
+    const topTen = scenario.rows.slice(0, 10);
+    return `
+      <button class="scenario-card" type="button" data-scenario-id="${html(scenario.id)}" style="--accent: ${CARD_ACCENTS[scenario.index % CARD_ACCENTS.length]}">
+        <span class="card-kicker">Outcome ${scenario.index + 1}</span>
+        <div>
+          <h3 class="card-title">${html(finalistText)}</h3>
+          <div class="winner-row">${teamMarkup(scenario.champion)}</div>
+        </div>
+          <div class="card-path">
+          <span>${html(scenario.semiOneWinner)} wins semi-final 1</span>
+          <span>${html(scenario.semiTwoWinner)} wins semi-final 2</span>
+        </div>
+        <div class="scenario-top" aria-label="Projected top 10">
+          <div class="scenario-top-head">
+            <span>Pos</span>
+            <span>Player</span>
+            <span>Pts</span>
+          </div>
+          ${topTen
+            .map(
+              (row) => `
+                <div class="scenario-top-row">
+                  <span class="scenario-rank">#${row.projectedRank}</span>
+                  <span class="scenario-player">${html(row.name)}<span class="scenario-mobile-points">${fmt(row.projected)}</span></span>
+                  <span class="scenario-total">${fmt(row.projected)}</span>
+                </div>
+              `
+            )
+            .join("")}
+        </div>
+      </button>
+    `;
+  }
+
+  function setActiveScenario(id, shouldPushUrl) {
+    const scenario = state.scenarios.find((item) => item.id === id) || state.scenarios[0];
+    if (!scenario) return;
+    state.activeId = scenario.id;
+
+    document.querySelectorAll(".scenario-card").forEach((card) => {
+      card.classList.toggle("is-active", card.dataset.scenarioId === scenario.id);
+    });
+
+    els.activeFinal.textContent = `${scenario.finalists[0]} x ${scenario.finalists[1]}`;
+    els.activeChampion.textContent = `${scenario.champion} lifts the trophy`;
+
+    if (shouldPushUrl) {
+      const url = new URL(window.location.href);
+      url.searchParams.set("scenario", scenario.id);
+      window.history.replaceState({}, "", url);
+    }
+  }
+
+  function renderScenarioGrid() {
+    els.scenarioGrid.innerHTML = state.scenarios.map(scenarioCard).join("");
+    els.scenarioGrid.addEventListener("click", (event) => {
+      const card = event.target.closest("[data-scenario-id]");
+      if (!card) return;
+      setActiveScenario(card.dataset.scenarioId, true);
+    });
+  }
+
+  function showToast(message) {
+    els.toast.textContent = message;
+    els.toast.classList.add("is-visible");
+    window.clearTimeout(showToast.timer);
+    showToast.timer = window.setTimeout(() => {
+      els.toast.classList.remove("is-visible");
+    }, 1800);
+  }
+
+  async function copyCurrentUrl() {
+    const url = new URL(window.location.href);
+    if (state.activeId) {
+      url.searchParams.set("scenario", state.activeId);
+    }
+    try {
+      if (navigator.share) {
+        await navigator.share({
+          title: "World Cup Pool: Final Scenarios",
+          text: "Eight possible paths for the prediction pool.",
+          url: url.toString(),
+        });
+        return;
+      }
+      await navigator.clipboard.writeText(url.toString());
+      showToast("Share link copied");
+    } catch (error) {
+      try {
+        await navigator.clipboard.writeText(url.toString());
+        showToast("Share link copied");
+      } catch (copyError) {
+        showToast("Could not copy link");
+      }
+    }
+  }
+
+  async function init() {
+    renderTeamLines();
+    els.scenarioGrid.innerHTML = '<div class="loading">Loading simulation snapshot...</div>';
+    els.copyPageBtn.addEventListener("click", copyCurrentUrl);
+
+    if (window.FINAL_SCENARIO_SNAPSHOT?.scenarios?.length) {
+      state.scenarios = window.FINAL_SCENARIO_SNAPSHOT.scenarios;
+      els.versionStamp.textContent = shortVersion(window.FINAL_SCENARIO_SNAPSHOT.version);
+      renderReadyState();
+    } else {
+      els.scenarioGrid.innerHTML =
+        '<div class="error">Could not load the simulation snapshot.</div>';
+    }
+  }
+
+  function renderReadyState() {
+    renderScenarioGrid();
+    const requested = new URLSearchParams(window.location.search).get("scenario");
+    const initial = state.scenarios.some((scenario) => scenario.id === requested)
+      ? requested
+      : state.scenarios[0].id;
+    setActiveScenario(initial, Boolean(requested));
+  }
+
+  init();
+})();
